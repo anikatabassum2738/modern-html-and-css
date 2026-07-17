5c35b1a4ab9fbc3c4cd49c6d17945561502f214a
Anika Tabassum <anikatabassum2738@gmail.com>
2026-07-18 00:01:22 +0600

    sticky nav & style on scroll, calc() Function

---
+++ HTML/08-various-css-features/04-sticky-navbar/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('.header')
+
+    function toggleHeaderTransparency() {
+        // console.log(123);
+        // console.log(window.scrollY);
+        if (window.scrollY > 0) {
+            header.classList.add('transparent')
+        } else {
+            header.classList.remove('transparent')
+        }
+    }
+
+    window.addEventListener('scroll', toggleHeaderTransparency);
+})
